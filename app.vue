694e3c11f831a2829bc19a7d6542212bf1f75268
Waradu <waradu@outlook.com>
2024-09-21 21:30:49 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+@font-face {
+  font-family: Inter;
+  src: url("assets/Inter.woff2") format("woff2");
+  font-display: swap;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+html,
+body,
+#__nuxt {
+  width: 100%;
+  height: 100%;
+  font-family: Inter, sans-serif;
+  background-color: #f5f5f5;
+  color: black;
+  transition: all .2s !important;
+
+  &:has(main.dark) {
+    background-color: #0c0c0c;
+    color: white;
+  }
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,64 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["nuxt-svgo", "@nuxt/image"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ["legacy-js-api"],
+        },
+      },
+    },
+  },
+  svgo: {
+    autoImportPath: false,
+  },
+  app: {
+    head: {
+      htmlAttrs: { lang: "en" },
+      title: "a.ni.me",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        { name: "author", content: "Waradu" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Can't keep up with all the anime you've watched? Neither can we! Organize your list, hide the for research purpose ones and keep things under control. We won't tell.",
+        },
+        {
+          property: "og:title",
+          content: "a.ni.me",
+        },
+        {
+          property: "og:description",
+          content:
+            "Can't keep up with all the anime you've watched? Neither can we! Organize your list, hide the for research purpose ones and keep things under control. We won't tell.",
+        },
+        {
+          property: "og:image",
+          content: "/icon.png",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:url",
+          content: "https://anime.waradu.dev",
+        },
+      ],
+    },
+  },
+  nitro: {
+    routeRules: {
+      "/images/**": { headers: { "cache-control": "max-age=86400" } },
+    },
+  },
+});
